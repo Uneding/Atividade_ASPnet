ac57c6686da312ec0cfb2f7c7d487715b0f7be86
João <joaopaulo102003@gmail.com>
2025-07-13 14:12:49 -0300

    Config Pastas

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,8 @@
+{
+public AppDbContext(DbContextOptions<AppDbContext> options)
+: base(options)
+{
+}
+// DbSets personalizados, se quiser adicionar depois
+// public DbSet<Algo> Algos { get; set; }
+}
